40e9c8f29f1107bf1ed6643a9f18c923eecfa345
dockerflask <dockerflask@protonmail.com>
2021-10-25 16:18:04 +0300

    Fix img tag, add logging, liveness and readiness

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, get_flashed_messages
 import os
+import logging
 import redis
 import secrets
 
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 app.secret_key = secrets.token_urlsafe(32)
 r = redis.Redis(host=os.environ['REDIS_HOST'],
                 port=os.environ['REDIS_PORT'],
@@ -25,22 +27,22 @@ def post():
 @app.route('/get', methods=['GET', 'POST'])
 def get():
     if request.method == 'POST':
-        return redirect(f"get_value/{request.form['key']}")
+        return redirect(f'get_value/{request.form["key"]}')
     return render_template('get.html')
 
 
 @app.route('/get_value/<key>', methods=['GET'])
 def get_value(key):
     flash(f'Key:{key} Value:{r.get(key).decode()}')
-    print(f'Debug msg (Get) Key:{key} Value:{r.get(key).decode()}')
+    app.logger.info(f'Debug msg (Get) Key:{key} Value:{r.get(key).decode()}')
     return redirect(url_for('index'))
 
 
 @app.route('/add_value', methods=['POST'])
 def add_value():
     r.set(request.form['key'], request.form['value'])
-    flash(f"The Pair {request.form['key']}:{request.form['value']} has been stored successfully!")
-    print(f"Debug msg (Post) Key:{request.form['key']} Value:{request.form['value']}")
+    flash(f'The Pair {request.form["key"]}:{request.form["value"]} has been stored successfully!')
+    app.logger.info(f'Debug msg (Post) Key:{request.form["key"]} Value:{request.form["value"]}')
     return redirect(url_for('index'))
 
 
